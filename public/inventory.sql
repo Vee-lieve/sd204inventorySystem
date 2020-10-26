7d1efda3308583ef4dc4ff3acfe961933d6fdd72
Queenissa <atacador.quency@gmail.com>
2020-10-26 18:06:22 +0800

    login page

--- public/inventory.sql
+++ public/inventory.sql
@@ -4,18 +4,10 @@ SET time_zone = "+00:00";
 
 -- table structure for user registration
 CREATE TABLE `register` (
-  `username` varchar(20) NOT NULL,
-  `first_name` varchar(30) NOT NULL,
-  `last_name` varchar(30) NOT NULL,
-  `mobile` int(15) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `password_1` varchar(50) NOT NULL
+  `username` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `password_1` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `register`
---
-
-INSERT INTO `register` (`username`, `first_name`, `last_name`, `mobile`, `email`, `password_1`) VALUES
-('frpartho', 'Partho', 'Bala', 1686998128, 'parthobala019@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b');
 
