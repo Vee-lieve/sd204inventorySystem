ee7deede13aa9bd9aa93ee74ab54cfedfea3b641
=vangie <=evegabayan16@gmail.com>
2020-10-31 17:11:24 +0800

    Modified form

--- public/register.php
+++ public/register.php
@@ -16,31 +16,56 @@
         <div class="container">
           <div class="row">
             <div class="col-md-9 col-lg-8 mx-auto">
-              <h3 class="register-heading mb-4">Register</h3>
-              <form>
-                <div class="form-label-group">
-                    <input type="text" id="inputUserame" class="form-control" placeholder="Username" required autofocus>
-                    <label for="inputUserame">Username</label>
+              <div class="login-wrap">
+	              <div class="login-html">  
+                  <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
+                  <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+                  <div class="login-form">
+                    <div class="sign-in-htm">
+                      <div class="group">
+                        <input type="text" id="user" class="input" placeholder="Username" required autofocus>
+                       
+                      </div>
+                      <div class="group">
+                        <input type="password" id="pass" class="input" data-type="password" placeholder="Password" required>
+                        
+                      </div>
+                      <div class="group">
+                        <input id="check" type="checkbox" class="check" checked>
+                        <label for="check"><span class="icon"></span> Keep me Signed in</label>
+                      </div>
+                      <div class="group">
+                        <input type="submit" class="button" value="Sign In">
+                      </div>
+                      <div class="hr"></div>
+                      <div class="foot-lnk">
+                        <a href="#forgot">Forgot Password?</a>
+                      </div>
+                    </div>
+                    <div class="sign-up-htm">
+                      <div class="group">
+                        <input type="text" id="user" class="input" placeholder="Username" required autofocus>
+                      </div>
+                      <div class="group">
+                        <input  type="password"  id="pass" class="input" data-type="password" placeholder="Password" required>
+                      </div>
+                      <div class="group">
+                        <input type="password" id="pass" class="input" data-type="password" placeholder="Confirm Password" required>
+                      </div>
+                      <div class="group">
+                        <input type="email" id="pass" class="input" placeholder="Email address" required>
+                      </div>
+                      <div class="group">
+                        <input type="submit" class="button" value="Sign Up">
+                      </div>
+                      <div class="hr"></div>
+                      <div class="foot-lnk">
+                        <label for="tab-1">Already Member?</a>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-
-                <div class="form-label-group">
-                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
-                    <label for="inputEmail">Email address</label>
-                </div>
-
-                <div class="form-label-group">
-                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                    <label for="inputPassword">Password</label>
-                </div>
-
-                <div class="form-label-group">
-                    <input type="password" id="inputConfirmPassword" class="form-control" placeholder="Password" required>
-                    <label for="inputConfirmPassword">Confirm password</label>
-                </div>
-
-                <button class="btn btn-lg btn-primary btn-block btn-register text-uppercase font-weight-bold mb-2" type="submit">Register</button>
-                <a class="d-block text-center mt-2 small" href="#">Sign In</a>
-              </form>
+              </div>
             </div>
           </div>
         </div>
