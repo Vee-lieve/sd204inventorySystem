260be3df95d75b44a46b74066e7ecc1cb8e78696
=vangie <=evegabayan16@gmail.com>
2020-11-03 11:47:52 +0800

    Modified Pages

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path')
 const app = express();
 
 //const atlasUrl = 'mongodb://Vee:p@ssw0rd@cluster0-shard-00-00.kw84b.mongodb.net:27017,cluster0-shard-00-01.kw84b.mongodb.net:27017,cluster0-shard-00-02.kw84b.mongodb.net:27017/Vee?ssl=true&replicaSet=atlas-pojndz-shard-0&authSource=admin&retryWrites=true&w=majority'
-const localUrl = 'mongodb://127.0.0.1/asmsi'
+const localUrl = 'mongodb://127.0.0.1/inventory'
 
 const useAtlas = false
 let url = useAtlas ? atlasUrl : localUrl
@@ -39,14 +39,14 @@ app.use(userRoute)
 //         sms: "Hello from express."
 //     })
 // })
+app.get('/register', function (req, res) {
+    res.sendFile(path.join(__dirname+'/views/register.html'));
+  });
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/views/index.html'));
   });
 
-//   app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname+'/views/home.html'));
-//   });
 var port = process.env.PORT || 8084
 const server = app.listen(port, function() {
     console.log('app listening on post:', port);
